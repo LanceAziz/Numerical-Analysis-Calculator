38ac25947fdc34b385783cbe78ed8d5873d79c76
Samer <samer.92835@cs.mti.edu.eg>
2023-04-28 01:49:52 +0300

    Working Bisection & False Position

--- js/main.js
+++ js/main.js
@@ -1,63 +1,51 @@
+// Clicking on each Method to show its form and hide the other forms
 document.querySelector("#Bisection").addEventListener("click", function () {
   document.querySelector("#Bis-Div").classList.remove("d-none");
   document.querySelector("#False-Div").classList.add("d-none");
   document.querySelector("#Simple-Div").classList.add("d-none");
   document.querySelector("#Newton-Div").classList.add("d-none");
   document.querySelector("#Scant-Div").classList.add("d-none");
-  let checkedMethod = document.querySelector(
-    'input[name="Method"]:checked'
-  ).value;
-  console.log(checkedMethod);
+  // Showing the chosen function in console (test)**********
+  console.log(document.querySelector('input[name="Method"]:checked').value);
 });
-
 document.querySelector("#FalsePos").addEventListener("click", function () {
   document.querySelector("#Bis-Div").classList.add("d-none");
   document.querySelector("#False-Div").classList.remove("d-none");
   document.querySelector("#Simple-Div").classList.add("d-none");
   document.querySelector("#Newton-Div").classList.add("d-none");
   document.querySelector("#Scant-Div").classList.add("d-none");
-  let checkedMethod = document.querySelector(
-    'input[name="Method"]:checked'
-  ).value;
-  console.log(checkedMethod);
+  // Showing the chosen function in console (test)**********
+  console.log(document.querySelector('input[name="Method"]:checked').value);
 });
-
 document.querySelector("#FixedPoint").addEventListener("click", function () {
   document.querySelector("#Bis-Div").classList.add("d-none");
   document.querySelector("#False-Div").classList.add("d-none");
   document.querySelector("#Simple-Div").classList.remove("d-none");
   document.querySelector("#Newton-Div").classList.add("d-none");
   document.querySelector("#Scant-Div").classList.add("d-none");
-  let checkedMethod = document.querySelector(
-    'input[name="Method"]:checked'
-  ).value;
-  console.log(checkedMethod);
+  // Showing the chosen function in console (test)**********
+  console.log(document.querySelector('input[name="Method"]:checked').value);
 });
-
 document.querySelector("#Newton").addEventListener("click", function () {
   document.querySelector("#Bis-Div").classList.add("d-none");
   document.querySelector("#False-Div").classList.add("d-none");
   document.querySelector("#Simple-Div").classList.add("d-none");
   document.querySelector("#Newton-Div").classList.remove("d-none");
   document.querySelector("#Scant-Div").classList.add("d-none");
-  let checkedMethod = document.querySelector(
-    'input[name="Method"]:checked'
-  ).value;
-  console.log(checkedMethod);
+  // Showing the chosen function in console (test)**********
+  console.log(document.querySelector('input[name="Method"]:checked').value);
 });
-
 document.querySelector("#Scant").addEventListener("click", function () {
   document.querySelector("#Bis-Div").classList.add("d-none");
   document.querySelector("#False-Div").classList.add("d-none");
   document.querySelector("#Simple-Div").classList.add("d-none");
   document.querySelector("#Newton-Div").classList.add("d-none");
   document.querySelector("#Scant-Div").classList.remove("d-none");
-  let checkedMethod = document.querySelector(
-    'input[name="Method"]:checked'
-  ).value;
-  console.log(checkedMethod);
+  // Showing the chosen function in console (test)**********
+  console.log(document.querySelector('input[name="Method"]:checked').value);
 });
 
+// Calls the method function based on the chosen method
 function Calculate() {
   let checkedMethod = document.querySelector(
     'input[name="Method"]:checked'
@@ -69,47 +57,59 @@ function Calculate() {
     case "FalsePos":
       BisAndFalse(checkedMethod);
       break;
+    case "FixedPoint":
+      break;
+    case "Newton":
+      break;
+    case "Scant":
+      break;
     default:
       break;
   }
 }
 
-function vc(x) {
-  return document.getElementById("fx").value.toLowerCase().replaceAll("x", x);
+// Equation solver
+function f(x, scope) {
+  return math.evaluate(
+    document.getElementById("fx").value.toLowerCase().replaceAll("x", x),
+    scope
+  );
 }
 
+// Bisection and False Position function based on the chosen method
 function BisAndFalse(method) {
   let XL, XU;
   if (method == "Bisection") {
-    XL = Number(document.getElementById("Bis-Xl").value);
-    XU = Number(document.getElementById("Bis-Xu").value);
+    XL = document.getElementById("Bis-Xl").value;
+    XU = document.getElementById("Bis-Xu").value;
   } else {
-    XL = Number(document.getElementById("False-Xl").value);
-    XU = Number(document.getElementById("False-Xu").value);
+    XL = document.getElementById("False-Xl").value;
+    XU = document.getElementById("False-Xu").value;
   }
   let scope = {
-    xl: XL,
-    xu: XU,
+    xl: Number(XL),
+    xu: Number(XU),
     xr: 0,
   };
+  let fxl = f("xl", scope);
+  let fxu = f("xu", scope);
   console.log(scope);
-  let fxl = math.evaluate(vc("xl"), scope);
-  let fxu = math.evaluate(vc("xu"), scope);
+  console.log(fxl);
+  console.log(fxu);
   if (fxl * fxu < 0) {
-    let err = document.getElementById("err").value;
-    console.log(method);
-    if (method == "Bisection") scope.xr = (scope.xl + scope.xu) / 2;
-    else scope.xr = scope.xu - (fxu * (scope.xl - scope.xu)) / (fxl - fxu);
-    let fxr = math.evaluate(vc("xr"), scope);
-    let currError = 100;
-    let resultList = [];
+    let currError = 100,
+      result,
+      resultList = [],
+      err = document.getElementById("err").value;
     for (let i = 0; currError > err; i++) {
-      scope.xr = (scope.xl + scope.xu) / 2;
-      fxr = math.evaluate(this.vc("xr"), scope);
+      console.log(method);
+      if (method == "Bisection") scope.xr = (scope.xl + scope.xu) / 2;
+      else scope.xr = scope.xu - (fxu * (scope.xl - scope.xu)) / (fxl - fxu);
+      fxr = f("xr", scope);
       if (i != 0)
         currError =
           math.abs((scope.xr - resultList[i - 1].xr) / scope.xr) * 100;
-      let result = {
+      result = {
         i: i,
         xl: scope.xl,
         fxl: fxl,
@@ -135,5 +135,28 @@ function BisAndFalse(method) {
   }
 }
 
+// Simple Fixed Point function
+function FixedPoint() {}
+
+// Newton Function
+function Newton() {
+  let scope = {
+    xi: Number(document.getElementById("Newton-Xi").value),
+  };
+
+}
+
+// Scant Function
+function Scant() {}
+
+// Test Cases
+// Bisection
+//
+// False Position
+// f(x) = -26 + 82.3x - 88x^2 + 45.4x^3 - 9x^4 + 0.65x^5, e = 0.2%, xl = 0.5, xu = 1 (Works)
+// f(x) = -13 - 20x + 19x^2 - 3x^3, e = 1%, xl = -1, xu = 0 (Works)
+
+
+
 //-2+7x-5x^2+6x^3
-//-26+82.3x - 88x^2 + 45.4x^3 - 9x^4 + 0.65x^5
+
